67ca1a23e60392e7836cb93637638cd89c2a7a2f
Stackbit Code Editor <projects@stackbit.com>
2021-03-16 14:58:55 +0000

    product.js: changed

--- src/templates/product.js
+++ src/templates/product.js
@@ -51,7 +51,7 @@ export default class Product extends React.Component {
                                 );
                             })())}
                             <div className="product__price">
-                                ${_.get(this.props, 'pageContext.frontmatter.price', null)}
+                                {_.get(this.props, 'pageContext.frontmatter.price', null)}
                             </div>
                             <article className="product__description">
                                 {_.get(this.props, 'pageContext.frontmatter.description', null)}
