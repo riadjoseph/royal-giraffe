d19ffedd9f5a258a8eb9ca3e34cfdbc21899d68f
Stackbit Code Editor <projects@stackbit.com>
2021-03-17 10:15:50 +0000

    ContactSection.js: changed

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -34,7 +34,7 @@ export default class ContactSection extends React.Component {
     <label>Email <input type="email" name="email" /></label>
   </p>
   <p>
-    <label>Email <input type="email" name="email" /></label>
+    <label>Message <input type="email" name="email" /></label>
   </p>
   <p>
     <button type="submit">Send</button>
