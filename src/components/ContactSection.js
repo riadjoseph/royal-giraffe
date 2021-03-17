238ac48afd804a39872800fe84a1912cef6b3c40
Stackbit Code Editor <projects@stackbit.com>
2021-03-17 11:41:15 +0000

    ContactSection.js: changed

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -32,7 +32,7 @@ export default class ContactSection extends React.Component {
                   )} 
                   <div/>
                 </div>
-              </div>
+             
             </section>
         );
     }
