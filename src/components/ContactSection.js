8c8eac6df9fc96104978545ae1f69abf16a9ed0a
Stackbit Code Editor <projects@stackbit.com>
2021-03-17 11:33:24 +0000

    ContactSection.js: changed

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -31,6 +31,7 @@ export default class ContactSection extends React.Component {
                     </svg>
                   </Link>
                   )} 
+                  </div>
                 </div>
               </div>
             </section>
