9ffbe5d32f444a8bf39701e9fd86aae594960154
Stackbit Code Editor <projects@stackbit.com>
2021-03-17 11:38:05 +0000

    ContactSection.js: changed

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -10,7 +10,7 @@ export default class ContactSection extends React.Component {
             <section className="content__row content__row--full-width content__row--mb-0  faq__contact"  data-id={_.get(section, 'section_id', null)}>
               {_.get(section, 'image', null) && (<img src={withPrefix(_.get(section, 'image', null))} alt="" className="faq__contact-image"/>)}
               <div className="faq__contact-text-container">
-                <p className="faq__contact-subtitle">You might have questions along the lines of "what happens when"... <br/> Let's have a chat.</p>
+                <p className="faq__contact-subtitle">You might have questions along the lines of "what happens now"... <br/> Let's have a chat.</p>
                 <div className="faq__contact-info-container">
                   <p className="faq__contact-telephone">{_.get(section, 'phone', null)}</p>
                   <p className="faq__contact-email">{_.get(section, 'email', null)}</p>
