4953dc0011e67d3bfe30f19a6cb11c95e947fc46
Stackbit Code Editor <projects@stackbit.com>
2021-03-17 10:25:36 +0000

    ContactSection.js: changed

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -26,20 +26,6 @@ export default class ContactSection extends React.Component {
                   )}
                 </div>
               </div>
-              <form class="form" name="contact" netlify>
-  <p> 
-    <label>Name <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Email <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Message <input type="paragraph" name="message" /></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
             </section>
         );
     }
