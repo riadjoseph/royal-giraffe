58c33d56c13603c6f05c25a6482559319caf9f23
Stackbit Code Editor <projects@stackbit.com>
2021-04-25 20:32:52 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -12,7 +12,7 @@ export default class HeroSection extends React.Component {
                 {_.map(_.get(section, 'actions', null), (action, action_idx) => (
                     <Link key={action_idx} to={withPrefix(_.get(action, 'url', null))} className="link hero__link">
                       {_.get(action, 'title', null)}
-                      {_.get(action, 'arrow', null) && (<svg width="26" height="14" viewBox="0 0 26 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M22.6819 6.07886H0V7.79048H22.8139L18.2402 12.3182L19.434 13.5L26 7L19.434 0.5L18.2402 1.68182L22.6819 6.07886Z" fill="#88DD9B"/></svg>)}
+                      {_.get(action, 'arrow', null) && (<svg width="26" height="14" viewBox="0 0 26 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M22.6819 6.07886H0V7.79048H22.8139L18.2402 12.3182L19.434 13.5L26 7L19.434 0.5L18.2402 1.68182L22.6819 6.07886Z" fill="#fff"/></svg>)}
                     </Link>
                 ))}
             </section>
